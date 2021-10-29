6c027cc0d7dd78acc1f11732d8523b565c6620b8
lucas874 <lsc500@outlook.dk>
2021-10-29 11:27:03 +0200

    :)

--- garden_turns/core/src/com/garden/game/world/Unit.java
+++ garden_turns/core/src/com/garden/game/world/Unit.java
@@ -18,9 +18,6 @@ import java.util.ArrayList;
 // turn in to interface? actor?? plants and character implement this??
 public class Unit extends Actor {
     GardenGame app;
-    Point position; // Tile position... not really used now
-    Vector2 pos;
-    float angle_old;
     String assetName;
     Sprite sprite;
     public int maxX, minX, maxY, minY;
@@ -34,22 +31,20 @@ public class Unit extends Actor {
     public Unit(GardenGame app, String assetName) {
         this.app = app;
         this.assetName = assetName;
-        position = new Point(0,0);
-        pos = new Vector2();
         this.sprite = app.assets.textureAtlas.createSprite("character000");
         this.walkAnimations = app.assets.walkAnimations;
         maxX = 32; // Hardcoded...
         maxY = 32;
         minX = 0;
         minY = 0;
-        angle_old = 0;
         activeAnimation = walkAnimations.get(0);
-        setPosition(0,0);
+        setX(16*32);
+        setY(16*32);
 
     }
 
     public void move(double x, double y) {
-        position.setLocation(x, y);
+        //position.setLocation(x, y);
     }
     public boolean canMove(int x, int y) {
         return (x < maxX && y < maxY && x >= minX && y >= minY);
@@ -59,8 +54,7 @@ public class Unit extends Actor {
     public void draw(Batch batch, float parentAlpha) {
         //super.draw(batch, parentAlpha);
         elapsedTime += Gdx.graphics.getDeltaTime();
-        //sprite.draw(batch);
-        //batch.draw(activeAnimation.getKeyFrame(elapsedTime, true), getX(), getY());
+
         batch.draw(drawThis, getX(), getY());
 
     }
@@ -73,17 +67,12 @@ public class Unit extends Actor {
 
     @Override
     public void setPosition(float x, float y) {
-        pos.set(x,y);
-        //System.out.println("angle - angle_old: " + String.valueOf(pos.angleDeg() - angle_old));
-        //System.out.println(pos.angleDeg());
-        //angle_old = pos.angleDeg();
-        //System.out.println(getX() + " " + x);
 
-        // Find a cleaner mapping to right animation.
+        // Find a cleaner mapping to right animation. Consider also rotating sprite...
         Vector2 route = new Vector2(x-getX(), y-getY());
         float distance = route.len();
-        //System.out.println("length: " + distance);
-        //System.out.println("angle: " + route.angleDeg());
+        System.out.println("length: " + distance);
+        System.out.println("angle: " + route.angleDeg());
         float angle = route.angleDeg();
         if(angle <= 45 || angle > 315) {
             activeAnimation = walkAnimations.get(RIGHT);
@@ -95,43 +84,15 @@ public class Unit extends Actor {
             activeAnimation = walkAnimations.get(DOWN);
         }
 
-        /*if(y >= x) {
-            if(y <= getY()) {
-                activeAnimation = walkAnimations.get(DOWN);
-            }
-            else {
-                activeAnimation = walkAnimations.get(UP);
-            }
-        } else {
-            if(x < getX()) {
-                activeAnimation = walkAnimations.get(LEFT);
-            } else {
-                activeAnimation = walkAnimations.get(RIGHT);
-            }
-        }*/
 
         clearActions();
         MoveToAction moveToAction = new MoveToAction();
         moveToAction.setPosition(x, y);
-        float duration = (float) Math.sqrt(Math.pow(x-getX(),2) + Math.pow(y-getY(), 2))/100f;
+        float duration = (float) Math.sqrt(Math.pow(x-getX(), 2) + Math.pow(y-getY(), 2))/100f;
         moveToAction.setDuration(duration);
         addAction(moveToAction);
-        //System.out.println(getActions());
+        System.out.println(getActions());
     }
 
 }
 
-
-// Do this in unit
-                /*MoveToAction moveToAction = new MoveToAction();
-                moveToAction.setPosition(position.x, position.y);
-
-                float duration = (float) Math.sqrt(position.x*position.x+position.y*position.y)/75f;
-
-                moveToAction.setDuration(duration);
-                world.user.unit.addAction(moveToAction);*/
-
-
-// sprite.setPosition(x,y);
-        //System.out.println("angle: " + String.valueOf(pos.angleDeg() - angle_old));
-              //  angle_old = pos.angleDeg();
--- garden_turns/garden_turns/core/src/com/garden/game/screens/AbstractScreen.java
+++
@@ -1,9 +0,0 @@
-package com.garden.game.screens;
-
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.Screen;
-
-// Make this interface to get Screen and input functionality in screens.
-// https://stackoverflow.com/questions/41385107/libgdx-how-to-catch-back-key-when-stage-is-the-input-processor
-public abstract class AbstractScreen extends InputAdapter implements Screen {
-}

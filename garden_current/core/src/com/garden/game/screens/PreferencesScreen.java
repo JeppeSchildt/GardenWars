c89e6bf8fe710527440f7e4f3143667c2945a21b
TheGuardiaan <casperfevre@gmail.com>
2021-11-05 09:25:28 +0100

    BackButton funk i pre

--- garden_current/core/src/com/garden/game/screens/PreferencesScreen.java
+++ garden_current/core/src/com/garden/game/screens/PreferencesScreen.java
@@ -1,6 +1,7 @@
 package com.garden.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -182,13 +183,7 @@ public class PreferencesScreen implements Screen {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
 
-                app.sound.buttonMenueSound();
-
-                if (app.preferencesBool)
-                    app.setScreen(app.pauseScreen);
-                else{
-                     app.setScreen(app.titleScreen);
-                }
+                BackButton();
 
             }
         });
@@ -230,14 +225,26 @@ public class PreferencesScreen implements Screen {
     @Override
     public void render(float delta) {
 
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) BackButton();
+
+
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act();
         stage.draw();
+    }
 
 
+    public void BackButton(){
+        app.sound.buttonMenueSound();
 
+        if (app.preferencesBool)
+            app.setScreen(app.pauseScreen);
+        else{
+            app.setScreen(app.titleScreen);
+        }
     }
 
     @Override

14f7e47d7eb22469ecc640f3e053d73be4221f90
lucas874 <lsc500@outlook.dk>
2021-12-20 16:48:27 +0100

    XXX

--- garden_2311_working/core/src/com/garden/game/screens/GameScreen.java
+++ garden_2311_working/core/src/com/garden/game/screens/GameScreen.java
@@ -791,15 +791,7 @@ public class GameScreen extends AbstractScreen {
                 DebugTable.remove();
             }
         }
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.X))
-        {
-            showDialog = true;
-            updateDialog(currentDialogList.get(0));
-            dialogBackground();
-        }
-
-
+        
     }
 
     void guidBoxSetup(){
